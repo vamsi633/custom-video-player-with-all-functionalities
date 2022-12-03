52536ba7eb91f6717bd1e1824bb9f007a66cf188
Vamsi krishna nouluri <92721694+vamsi633@users.noreply.github.com>
2022-12-03 12:05:31 +0530

    Add files via upload

---
+++ custom video player/app.js
@@ -0,0 +1,160 @@
+const container=document.querySelector('.container'),
+mainVideo=container.querySelector('video'),
+videoTimeline=container.querySelector('.video-timeline'),
+progressBar=container.querySelector('.progress-bar'),
+VolumeBtn=container.querySelector('.volume i'),
+volumeSlider=container.querySelector('.left input'),
+currentVidTime=container.querySelector('.current-time'),
+videoDuration=container.querySelector('.video-duration'),
+skipBackward=container.querySelector('.skip-backward i'),
+skipForward=container.querySelector('.skip-forward i'),
+playPauseBtn=container.querySelector('.play-pause i'),
+speedBtn=container.querySelector('.playback-speed span'),
+speedOptions=container.querySelector('.speed-options'),
+picInPicBtn=container.querySelector('.pic-in-pic span'),
+fullscreenBtn=container.querySelector('.fullscreen i');
+let timer;
+
+const hideControls=()=>{
+    if(mainVideo.paused) return;
+   timer=setTimeout(()=>{
+        container.classList.remove("show-controls");
+    },3000);
+}
+
+hideControls();
+
+container.addEventListener("mousemove",()=>{
+    container.classList.add("show-controls");
+    clearTimeout(timer);
+    hideControls();
+});
+
+const formatTime=time=>{
+    let seconds=Math.floor(time%60),
+    minutes=Math.floor(time/60)%60,
+    hours=Math.floor(time/3600);
+
+    seconds=seconds<10?`0${seconds}`: seconds;
+    minutes=minutes<10?`0${minutes}`: minutes;
+    hours=hours<10?`0${hours}`: hours;
+
+    if(hours==0){
+        return `${minutes}:${seconds}`;
+    }
+    return `${hours}:${minutes}:${seconds}`;
+}
+mainVideo.addEventListener("timeupdate", e => {
+    let { currentTime, duration }=e.target;
+    let percent=(currentTime/duration)*100;
+    progressBar.style.width=`${percent}%`;
+
+    currentVidTime.innerText=formatTime(currentTime);
+});
+
+mainVideo.addEventListener("loadeddata",e=>{
+    videoDuration.innerText=formatTime(e.target.duration);
+});
+
+videoTimeline.addEventListener("click",e=>{
+    let timelineWidth=videoTimeline.clientWidth;
+    mainVideo.currentTime=(e.offsetX/timelineWidth)*mainVideo.duration;
+});
+
+const draggableProgressBar=e=>{
+    let timelineWidth=videoTimeline.clientWidth;
+    progressBar.style.width=`${e.offsetX}px`;
+    mainVideo.currentTime=(e.offsetX/timelineWidth)*mainVideo.duration;
+
+
+    currentVidTime.innerText=formatTime(mainVideo.currentTime);
+}
+
+videoTimeline.addEventListener("mousedown",()=>{
+    videoTimeline.addEventListener("mousemove",draggableProgressBar);
+});
+
+container.addEventListener("mouseup",()=>{
+    videoTimeline.removeEventListener("mousemove",draggableProgressBar);
+});
+
+videoTimeline.addEventListener("mouseover",e=>{
+    const progresTime=videoTimeline.querySelector("span");
+    let offsetX=e.offsetX;
+    progresTime.style.left=`${offsetX}px`;
+    let timelineWidth=videoTimeline.clientWidth;
+    let percent=(e.offsetX/timelineWidth)*mainVideo.duration;
+    progresTime.innerText=formatTime(percent);
+});
+
+VolumeBtn.addEventListener("click",()=>{
+    if(!VolumeBtn.classList.contains("fa-volume-high")){
+        mainVideo.volume=0.5;
+        VolumeBtn.classList.replace("fa-volume-xmark","fa-volume-high");
+        
+    }else{
+        mainVideo.volume=0.0;
+        VolumeBtn.classList.replace("fa-volume-high","fa-volume-xmark");
+    }
+
+    volumeSlider.value=mainVideo.volume;  //update slider value according to video volume
+});
+
+volumeSlider.addEventListener("input",e=>{
+    mainVideo.volume=e.target.value;
+    if(e.target.value==0){
+        VolumeBtn.classList.replace("fa-volume-high","fa-volume-xmark");
+    }else{
+        VolumeBtn.classList.replace("fa-volume-xmark","fa-volume-high"); 
+    }
+});
+
+
+speedBtn.addEventListener("click",()=>{
+    speedOptions.classList.toggle("show");
+});
+
+speedOptions.querySelectorAll("li").forEach(option=>{
+    option.addEventListener("click",()=>{
+        mainVideo.playbackRate=option.dataset.speed; //passing option dataset value as play back speed value
+        speedOptions.querySelector(".active").classList.remove("active");
+        option.classList.add("active");
+    });
+});
+
+document.addEventListener("click",e=>{  //hide speed options on document click
+    if(e.target.tagName!=="SPAN" || e.target.className!=="material-symbols-rounded"){
+        speedOptions.classList.remove("show");
+    }
+});
+
+picInPicBtn.addEventListener("click",()=>{
+    mainVideo.requestPictureInPicture();
+});
+
+fullscreenBtn.addEventListener("click",()=>{
+    container.classList.toggle("fullscreen");
+    if(document.fullscreenElement){
+        fullscreenBtn.classList.replace("fa-compress","fa-expand");
+        return document.exitFullscreen;
+    }
+    fullscreenBtn.classList.replace("fa-expand","fa-compress");
+    document.requestFullScreen();
+});
+
+skipBackward.addEventListener("click",()=>{
+    mainVideo.currentTime-=5;
+});
+skipForward.addEventListener("click",()=>{
+    mainVideo.currentTime+=5;
+});
+playPauseBtn.addEventListener("click",()=>{
+    mainVideo.paused ? mainVideo.play() : mainVideo.pause();
+});
+
+mainVideo.addEventListener("play",()=>{
+    playPauseBtn.classList.replace("fa-play","fa-pause");
+});
+mainVideo.addEventListener("pause",()=>{
+    playPauseBtn.classList.replace("fa-pause","fa-play");
+});
